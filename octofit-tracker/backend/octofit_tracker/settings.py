8c5b87a3ddd5bcaca9a3afe666b0b5a379904000
AronCali <91116089+Apmcalimag2003@users.noreply.github.com>
2025-11-27 03:34:25 +0000

    space

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -139,3 +139,4 @@ CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
